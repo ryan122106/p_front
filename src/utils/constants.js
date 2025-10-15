674100407300500b3571e5e6dde4ad09471b0440
Ryan <ryan.sensei21@gmail.com>
2025-10-15 16:39:56 +0800

    c

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-// src/utils/constants.js
-export const API_URL = "http://localhost:5123"; // <-- notice trailing slash
+
+export const API_URL = import.meta.env.DEV ? 
+"https://localhost:5123/api/" : "https://b15-piriyan.mak3r.dev/api/";
