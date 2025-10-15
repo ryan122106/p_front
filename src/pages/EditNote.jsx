444eb008176558973a109d1a2f8a21ba562c3f92
Ryan <ryan.sensei21@gmail.com>
2025-10-16 03:02:48 +0800

    c

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -95,7 +95,7 @@ const EditNote = () => {
     const formData = new FormData();
     files.forEach((file) => formData.append("media", file));
 
-    const res = await axios.post("http://localhost:5123/api/image", formData, {
+    const res = await axios.post(`${API_URL}/api/image`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${token}`,
